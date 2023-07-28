6944e198826aafff59c3b8007a31887d015bdb7a
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-07-28 23:57:14 +0530

    Update code.cpp

--- Anagram Pairs/code.cpp
+++ Anagram Pairs/code.cpp
@@ -1,6 +1,22 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include<bits/stdc++.h>
+bool isAnagram(string str1, string str2)
+{
+    //Write your code here
+     int n=str1.size();
+     int m=str2.size();
+     if(n!=m){
+         return false;
+     }
+    map<char,int>m1;
+    map<char,int>m2;
+    for (int i = 0; i < n; i++) {
+        m1[str1[i]]++;
+    }
+    for (int i = 0; i < m; i++) {
+       
+        m2[str2[i]]++;
+    }
+   if (m1!=m2)
+     return false;
+   return true;
 }
