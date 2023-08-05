a5a171b8d6ce02933cef9a005ecbdf2e24a6f9f9
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-08-05 23:55:42 +0530

    Update DyanmicP.cpp

--- Frog Jump/DyanmicP.cpp
+++ Frog Jump/DyanmicP.cpp
@@ -1,6 +1,19 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <bits/stdc++.h> 
+int fun(int index,vector<int> &heights, vector<int>&dp){
+    if(index==0){
+        return 0;
+    }
+    if(dp[index]!=-1)
+    return dp[index];
+    int left=fun(index-1,heights, dp)+abs(heights[index]-heights[index-1]);
+    int right=INT_MAX;
+    if(index>1)
+    right=fun(index-2,heights, dp)+abs(heights[index]-heights[index-2]);
+    return dp[index]=min(left,right);
+}
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int>dp(n+1,-1);
+    return fun(n-1, heights,dp);
 }
