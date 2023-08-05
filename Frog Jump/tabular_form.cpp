d726b388e3f5a891765f55e93aa64440135e2be8
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-08-05 23:56:35 +0530

    Update tabular_form.cpp

--- Frog Jump/tabular_form.cpp
+++ Frog Jump/tabular_form.cpp
@@ -1,6 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <bits/stdc++.h> 
+int frogJump(int n, vector<int> &heights)
+{
+    // Write your code here.
+    vector<int>dp(n,-1);
+    dp[0]=0;
+    for(int i=1;i<n;i++){
+        int firststep=dp[i-1]+abs(heights[i]-heights[i-1]);
+        int secondstep=INT_MAX;
+        if(i>1){
+        secondstep=dp[i-2]+abs(heights[i]-heights[i-2]);
+        }
+        dp[i]=min(firststep,secondstep);
+    }
+    return dp[n-1];
 }
