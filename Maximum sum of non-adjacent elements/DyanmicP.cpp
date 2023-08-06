4fb32b24a12edede3f7f8646cbdcfe38598d1259
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-08-06 18:48:23 +0530

    Update DyanmicP.cpp

--- Maximum sum of non-adjacent elements/DyanmicP.cpp
+++ Maximum sum of non-adjacent elements/DyanmicP.cpp
@@ -1,6 +1,16 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#memoization
+#include <bits/stdc++.h> 
+int fun(int index,vector<int>&v1,vector<int>&dp){
+    if(index==0)return v1[index];
+    if(index<0)return 0;
+    if (dp[index]!=-1) return dp[index];
+    int pick=v1[index]+fun(index-2,v1,dp);
+    int notpick=0+fun(index-1,v1,dp);
+    return dp[index]=max(pick,notpick);
+}
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int n=nums.size();
+    vector<int>dp(n,-1);
+    return fun(n-1,nums,dp);
 }
