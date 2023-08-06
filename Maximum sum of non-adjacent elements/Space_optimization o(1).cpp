239d778be740f0ea9886cc96c205285381ea1c7e
Saloni Sharma <94220356+Salonishrma@users.noreply.github.com>
2023-08-06 18:48:59 +0530

    Update Space_optimization o(1).cpp

--- Maximum sum of non-adjacent elements/Space_optimization o(1).cpp
+++ Maximum sum of non-adjacent elements/Space_optimization o(1).cpp
@@ -1,6 +1,19 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2638
-\cocoatextscaling0\cocoaplatform0{\fonttbl}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\paperw11900\paperh16840\margl1440\margr1440\vieww11520\viewh8400\viewkind0
+#include <bits/stdc++.h> 
+
+int maximumNonAdjacentSum(vector<int> &nums){
+    // Write your code here.
+    int n=nums.size();
+    int prev1=nums[0];
+    int prev2=0;
+    for(int i=1;i<n;i++){
+        int take=nums[i];
+        if(i>1){
+            take+=prev2;
+        }
+        int nottake=0+prev1;
+        int curri=max(take,nottake);
+        prev2=prev1;
+        prev1=curri;
+    }
+    return prev1;
 }
